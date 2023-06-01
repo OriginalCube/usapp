cafc0181bdb44f779af798fba19a13969fdad0f2
OriginalCube <lancemiraflorcube@gmail.com>
2023-06-01 16:02:36 +0800

    Added if user exist

--- server/controllers/DatabaseController.js
+++ server/controllers/DatabaseController.js
@@ -1,12 +1,31 @@
 const db = require("../models/AccountModel");
 
 const createAccount = async (req, res) => {
-  const createAccount = await db.create(req.body);
-  res.status(201).json(createAccount);
+  try {
+    const { email, username } = req.body;
+    const usernameExist = await db.findOne({ username });
+    const emailExist = await db.findOne({ email });
+    if (!usernameExist && !emailExist) {
+      const createAccount = await db.create(req.body);
+      res.status(200).json({ message: "Successfully Created Account!" });
+    } else {
+      res.json({ message: "User already exist!" });
+    }
+  } catch (err) {}
 };
 
 const loginAccount = async (req, res) => {
-  res.status(200).json(req.body);
+  try {
+    const userExist = await db.findOne(req.body);
+    if (!userExist) {
+      console.log("does not exist");
+      res.json({ message: "User does not exist!" });
+    } else {
+      res.status(200).json({ message: "User does exist." });
+    }
+  } catch (err) {
+    console.log("db error");
+  }
 };
 
 module.exports = {
